538514a68490720ece96e03fc44ae3b3f03e41d9
Mitchell Rathgeb <megafrogg@gmail.com>
2017-08-09 14:46:06 -0400

    File cleanup and readme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
+
 
 class App extends Component {
   render() {
