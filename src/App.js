1746e87a558a4e7e2e2155372ea688904ed129a6
Mitchell Rathgeb <megafrogg@gmail.com>
2017-08-09 13:37:25 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './App.css';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+
+          <div className="user" id="Amanda">
+            <h2>Amanda</h2>
+            <ul className="chores" id="amanda-chores">
+              <li>Sweep some shit</li>
+            </ul>
+            <input type="text" className="textbox" name="chore" placeholder="Chore..." id="amanda-chore-text" required />
+            <input type="button" id="amanda-chore-button" value="Add Chore" />
+            <input type="button" id="amanda-chore-clear" value="Clear Done" />
+          </div>
+
+          <div className="user" id="Rob">
+            <h2>Rob</h2>
+            <ul className="chores" id="rob-chores">
+            </ul>
+            <form className="add-chore">
+              <input type="text" className="textbox" name="chore" placeholder="Chore..." required />
+              <input type="button" className="btn" value="Add Chore" />
+              <input type="button" id="amanda-chore-clear" value="Clear Done" />
+            </form>
+          </div>
+
+          <div className="user" id="Shannon">
+            <h2>Shannon</h2>
+            <ul className="chores" id="shannon-chores">
+            </ul>
+            <form className="add-chore">
+              <input type="text" className="textbox" name="chore" placeholder="Chore..." required />
+              <input type="button" className="btn" value="Add Chore" />
+              <input type="button" id="amanda-chore-clear" value="Clear Done" />
+            </form>
+          </div>
+
+          <div className="user" id="Mitchell">
+            <h2>Mitchell</h2>
+            <ul className="chores" id="mitchell-chores">
+            </ul>
+            <form className="add-chore">
+              <input type="text" className="textbox" name="chore" placeholder="Chore..." required />
+              <input type="button" className="btn" value="Add Chore" />
+              <input type="button" id="amanda-chore-clear" value="Clear Done" />
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
